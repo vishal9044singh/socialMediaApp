369c2ad58321a6b40acae3e6a132c0864e0694cf
Vishal Singh <vishal9044singh@gmail.com>
2023-03-25 20:07:11 +0530

    First Commit:Setup express server and excluded node_modules using .gitignore

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Error in connecting server!! ${err}`);
+        return;
+    }
+    console.log(`Server is successfully connected to port: ${port}`);
+});
